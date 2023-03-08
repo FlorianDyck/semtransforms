fb6be9b8fef644aab1a8abf4f6bfa8adcf72a642
Florian <florian-dyck@gmx.de>
2023-03-08 17:56:38 +0100

    - bug fix: --parallel with <= 4 CPUs would first map, then uimap - minor code quality improvements

--- mapreduce.py
+++ mapreduce.py
@@ -27,28 +27,23 @@ class JsonlSaver:
         self._update_handler()
 
     def _file_path(self):
-        return os.path.join(self.save_dir, "statistics-%d%s" % (self.file_count, self._file_ending))
+        return os.path.join(self.save_dir, f"statistics-{self.file_count}{self._file_ending}")
 
     def _find_unique_index(self):
         while os.path.exists(self._file_path()):
             self.file_count += 1
 
     def _open_file(self, file_path):
-        if self.gzip_compress:
-            return gzip.open(file_path, "wb")
-        else:
-            return open(file_path, "wb")
+        return (gzip.open if self.gzip_compress else open)(file_path, "wb")
 
     def _update_handler(self):
-        
+
         need_update = self.file_handler is None or self.object_count >= self.num_objects
         if not need_update: return
 
-        file_path = self._file_path()
-
         if self.file_handler is not None: self.file_handler.close()
 
-        self.file_handler = self._open_file(file_path)
+        self.file_handler = self._open_file(self._file_path())
         self.file_count += 1
         self.object_count = 0
 
@@ -68,11 +63,7 @@ class JsonlSaver:
 def jsonl_reduce_io(output_dir, compress = False):
     saver = JsonlSaver(output_dir, gzip_compress = compress)
     try:
-        
-        def call_save(obj):
-            saver.save(obj)
-
-        yield call_save
+        yield saver.save
     finally:
         saver.close()
 
@@ -86,21 +77,18 @@ def pmap(map_fn, data):
     if cpu_count <= 4: # Too few CPUs for multiprocessing
         for output in map(map_fn, data):
             yield output
-
-    with ProcessPool(processes = cpu_count) as pool:
-        for output in pool.uimap(map_fn, data, chunksize = 4 * cpu_count):
-            yield output
+    else:
+        with ProcessPool(processes = cpu_count) as pool:
+            for output in pool.uimap(map_fn, data, chunksize = 4 * cpu_count):
+                yield output
 
 # Helper ------------------------------------------------------------------
 
 
 def _reduce_mapped_instances(mapped_instance_stream, reducer_fn):
     # Reduce all mapped instances
-    for mapped_instances in mapped_instance_stream:
-        if mapped_instances is None: continue
-
-        for mapped_instance in mapped_instances:
-            reducer_fn(mapped_instance)
+    for mapped_instance in _reduce_generator(mapped_instance_stream):
+        reducer_fn(mapped_instance)
 
 
 def _reduce_to_file(mapped_instance_stream, dir_path, compress = False):
@@ -124,15 +112,15 @@ def mapreduce(data, map_fn, reducer_fn = None, parallel = False, compress = Fals
     """
     Map then reduce functions
     Output of map has to be always a collection
+
     reducer_fn == None: Same as pmap / map
+
     reducer_fn == file_path: Saves all entries to jsonl into a dir
+
     reducer_fn == callable : Calls reducer with the mapped results
     """
 
-    if parallel:
-        mapped_instance_stream = pmap(map_fn, data)
-    else:
-        mapped_instance_stream = map(map_fn, data)
+    mapped_instance_stream = (pmap if parallel else map)(map_fn, data)
 
     if report: mapped_instance_stream = tqdm(mapped_instance_stream, total = len(data))
 
